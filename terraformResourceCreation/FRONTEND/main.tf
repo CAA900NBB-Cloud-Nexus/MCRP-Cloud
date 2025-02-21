1a515924d554947d8839c353dfac31ba495e0a2a
shri-5279 <77598004+shri-5279@users.noreply.github.com>
2025-02-21 16:30:28 -0500

    Update - Frontend Code
    Frontend code - N/W modification

--- terraformResourceCreation/FRONTEND/main.tf
+++ terraformResourceCreation/FRONTEND/main.tf
@@ -11,3 +11,16 @@ resource "aws_ecr_repository" "frontend_repo" {
 resource "aws_ecs_cluster" "frontend_cluster" {
   name = var.ecs_cluster_name_ui
 }
+
+### **Associate Frontend ECS with the Existing Backend Network**
+data "aws_vpc" "existing_vpc" {
+  id = var.vpc_id  # Use existing VPC where backend is deployed
+}
+
+data "aws_subnet" "existing_subnet" {
+  id = var.subnet_id  # Use existing subnet
+}
+
+data "aws_security_group" "existing_sg" {
+  id = var.security_group_id  # Use existing security group
+}
--- terraformResourceCreation/FRONTEND/terraform.tfvars
+++ terraformResourceCreation/FRONTEND/terraform.tfvars
@@ -1,3 +1,8 @@
 region             = "us-east-1"
 ecr_repo_name_ui  = "mcrp-ui-image-repo"
 ecs_cluster_name_ui  = "mcrp-ui-ecs-cluster"
+
+# Use the same network as backend
+vpc_id             = "10.0.0.0/16"
+subnet_id          = "10.0.1.0/24"
+security_group_id  = "mcrp-hello-ecs-sg"
