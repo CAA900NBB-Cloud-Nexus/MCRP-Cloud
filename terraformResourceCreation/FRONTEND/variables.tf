5c2414cb9a6e9e0acb7eb53ce0cb5c848a568d49
shri-5279 <77598004+shri-5279@users.noreply.github.com>
2025-02-21 16:36:34 -0500

    variables.tf - Added
    Variables added

--- terraformResourceCreation/FRONTEND/variables.tf
+++ terraformResourceCreation/FRONTEND/variables.tf
@@ -9,3 +9,15 @@ variable "ecr_repo_name_ui" {
 variable "ecs_cluster_name_ui" {
   description = "Name of the Frontend ECS cluster"
 }
+
+variable "vpc_id" {
+  description = "ID of the existing VPC where backend is deployed"
+}
+
+variable "subnet_id" {
+  description = "ID of the existing subnet to use for frontend ECS"
+}
+
+variable "security_group_id" {
+  description = "ID of the existing security group"
+}
