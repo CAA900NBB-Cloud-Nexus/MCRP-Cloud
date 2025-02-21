62e6231dd83fa38a4123ff8658e8ecae2453f660
akshayaca <akshayaca@outlook.com>
2025-02-20 19:18:01 -0500

    FInalised code for resource deployment

--- terraformResourceCreation/outputs.tf
+++ terraformResourceCreation/outputs.tf
@@ -13,9 +13,3 @@ output "ecr_repository_url" {
 output "security_group_id" {
   value = aws_security_group.backend_sg.id
 }
-output "iam_instance_profile" {
-  value = aws_iam_instance_profile.ec2_profile.name
-}
-output "iam_role_name" {
-  value = aws_iam_role.ec2_role.name
-}
