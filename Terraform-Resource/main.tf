ef109637e9e42401efdc3b2e3404a0fc93a630ef
shri-5279 <77598004+shri-5279@users.noreply.github.com>
2025-03-07 13:54:45 -0500

    Update - main.tf
    Deleted additional features tab

--- Terraform-Resource/main.tf
+++ Terraform-Resource/main.tf
@@ -1,14 +1,8 @@
 provider "azurerm" {
   features {}
-}
-
-provider "azurerm" {
-  features {}
-
   # This enables Terraform to use the Azure CLI login
   use_msi = false
 }
-
 
 variable "resource_group_name" {
   default = "myResourceGroup"
