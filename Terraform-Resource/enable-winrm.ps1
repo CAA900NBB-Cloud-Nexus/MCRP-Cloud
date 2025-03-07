0f927f8a8c7e425bab6f8c18bf4966f8776f057f
shri-5279 <77598004+shri-5279@users.noreply.github.com>
2025-03-07 16:59:42 -0500

    Final update - WinRM
    Code accommodates WinRM restarts and works correctly.

--- Terraform-Resource/enable-winrm.ps1
+++ Terraform-Resource/enable-winrm.ps1
@@ -2,3 +2,4 @@ winrm quickconfig -q
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
 Enable-PSRemoting -Force
+Restart-Service WinRM
--- Terraform-Resource/install-docker.ps1
+++ Terraform-Resource/install-docker.ps1
@@ -1,34 +1,22 @@
-# Enable TLS 1.2 for secure downloads
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
-# Install Docker
-Invoke-WebRequest -Uri "https://desktop.docker.com/win/stable/Docker%20Desktop%20Installer.exe" -OutFile "C:\DockerDesktopInstaller.exe"
-Start-Process "C:\DockerDesktopInstaller.exe" -ArgumentList "/quiet" -Wait
-
-# Add Docker path
-$env:Path += ";C:\Program Files\Docker\Docker\resources\bin"
+# Install Docker Engine
+Install-WindowsFeature -Name Containers -IncludeAllSubFeature -IncludeManagementTools
+Start-Service Docker
 
 # Install AWS CLI
 Invoke-WebRequest -Uri "https://awscli.amazonaws.com/AWSCLIV2.msi" -OutFile "C:\AWSCLIV2.msi"
 Start-Process msiexec.exe -ArgumentList "/i C:\AWSCLIV2.msi /quiet /norestart" -Wait
 
-# Restart the machine to apply Docker installation
-Restart-Computer -Force
-
-# Wait for reboot
-Start-Sleep -s 60
-
 # Authenticate with AWS ECR
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 970547375353.dkr.ecr.us-east-1.amazonaws.com
 
-# Pull UI container from ECR
+# Pull UI container
 docker pull 970547375353.dkr.ecr.us-east-1.amazonaws.com/mcrp-ui-image-repo:latest
 
-# Pull API container from ECR
+# Pull API container
 docker pull 970547375353.dkr.ecr.us-east-1.amazonaws.com/mcrp-api-image-repo:latest
 
-# Run UI container on localhost (Port 80)
+# Run UI container
 docker run -d --name ui-container -p 80:80 --restart unless-stopped 970547375353.dkr.ecr.us-east-1.amazonaws.com/mcrp-ui-image-repo:latest
 
-# Run API container on localhost (Port 3000)
+# Run API container
 docker run -d --name api-container -p 3000:3000 --restart unless-stopped 970547375353.dkr.ecr.us-east-1.amazonaws.com/mcrp-api-image-repo:latest
