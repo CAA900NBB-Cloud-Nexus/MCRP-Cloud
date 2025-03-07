4650fea5c317184df271356368f8077f4223c006
shri-5279 <77598004+shri-5279@users.noreply.github.com>
2025-03-07 17:25:31 -0500

    Configuration file added
    Added configuration for the access keys to fetch and pull images from ECRs respectively

--- Terraform-Resource/install-docker.ps1
+++ Terraform-Resource/install-docker.ps1
@@ -9,21 +9,21 @@ Start-Process msiexec.exe -ArgumentList "/i AWSCLIV2.msi /quiet" -Wait
 
 # Set AWS Credentials (Replace with your AWS keys)
 Write-Output "Configuring AWS CLI..."
-aws configure set aws_access_key_id YOUR_AWS_ACCESS_KEY
-aws configure set aws_secret_access_key YOUR_AWS_SECRET_KEY
-aws configure set default.region YOUR_AWS_REGION
+aws configure set aws_access_key_id AKIA6D6JBVT4VRT3MGHM
+aws configure set aws_secret_access_key kjI+BuuqFEV0QMzOn4jMu3V6UhAdHk9FjtsdHMk9
+aws configure set default.region us-east-1
 
 # Authenticate Docker to AWS ECR
 Write-Output "Authenticating Docker with AWS ECR..."
-$ECR_LOGIN = aws ecr get-login-password --region YOUR_AWS_REGION
-docker login --username AWS --password-stdin YOUR_AWS_ACCOUNT_ID.dkr.ecr.YOUR_AWS_REGION.amazonaws.com
+$ECR_LOGIN = aws ecr get-login-password --region us-east-1
+docker login --username AWS --password-stdin 970547375353.dkr.ecr.us-east-1.amazonaws.com
 
 # Pull Docker Image from AWS ECR
 Write-Output "Pulling Docker Image from AWS ECR..."
-docker pull YOUR_AWS_ACCOUNT_ID.dkr.ecr.YOUR_AWS_REGION.amazonaws.com/YOUR_IMAGE_NAME:latest
+docker pull 970547375353.dkr.ecr.us-east-1.amazonaws.com/YOUR_IMAGE_NAME:latest
 
 # Run the Docker Container
 Write-Output "Starting Docker Container..."
-docker run -d -p 80:80 YOUR_AWS_ACCOUNT_ID.dkr.ecr.YOUR_AWS_REGION.amazonaws.com/YOUR_IMAGE_NAME:latest
+docker run -d -p 80:80 970547375353.dkr.ecr.us-east-1.amazonaws.com/YOUR_IMAGE_NAME:latest
 
 Write-Output "Docker setup complete!"
