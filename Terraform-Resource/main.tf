15b7b84396474798d012b0d8975845801b478fdb
shri-5279 <77598004+shri-5279@users.noreply.github.com>
2025-03-07 19:39:00 -0500

    Network interface - changes
    Removed network interface "NIC"

--- Terraform-Resource/main.tf
+++ Terraform-Resource/main.tf
@@ -9,27 +9,6 @@ resource "azurerm_resource_group" "rg" {
   location = "East US"
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "vm-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "public-ip-config"
-    subnet_id                     = azurerm_subnet.public_subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
-  }
-}
-
-resource "azurerm_public_ip" "vm_public_ip" {
-  name                = "vm-public-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
 resource "azurerm_virtual_network" "vnet" {
   name                = "my-vnet"
   location            = azurerm_resource_group.rg.location
@@ -74,6 +53,27 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
+resource "azurerm_public_ip" "vm_public_ip" {
+  name                = "vm-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "vm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "public-ip-config"
+    subnet_id                     = azurerm_subnet.public_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
+  }
+}
+
 resource "azurerm_network_interface_security_group_association" "nsg_association" {
   network_interface_id      = azurerm_network_interface.nic.id
   network_security_group_id = azurerm_network_security_group.nsg.id
@@ -94,54 +94,35 @@ resource "azurerm_windows_virtual_machine" "vm" {
   }
 
   custom_data = base64encode(<<EOF
-  # Install Docker on Windows Server
-  Write-Output "Installing Docker..."
-  Install-WindowsFeature -Name Containers
+# Install Docker on Windows Server
+Write-Output "Installing Docker..."
+Install-WindowsFeature -Name Containers
 
-  # Install AWS CLI
-  Write-Output "Installing AWS CLI..."
-  Invoke-WebRequest -Uri "https://awscli.amazonaws.com/AWSCLIV2.msi" -OutFile "AWSCLIV2.msi"
-  Start-Process msiexec.exe -ArgumentList "/i AWSCLIV2.msi /quiet" -Wait
-  Write-Output "AWS CLI Installed."
+# Install AWS CLI
+Write-Output "Installing AWS CLI..."
+Invoke-WebRequest -Uri "https://awscli.amazonaws.com/AWSCLIV2.msi" -OutFile "AWSCLIV2.msi"
+Start-Process msiexec.exe -ArgumentList "/i AWSCLIV2.msi /quiet" -Wait
+Write-Output "AWS CLI Installed."
 
-  # Authenticate Docker with AWS ECR
-  Write-Output "Authenticating Docker with AWS ECR..."
-  $ECR_LOGIN = aws ecr get-login-password --region us-east-1
-  docker login --username AWS --password-stdin 970547375353.dkr.ecr.us-east-1.amazonaws.com
+# Authenticate Docker with AWS ECR
+Write-Output "Authenticating Docker with AWS ECR..."
+$ECR_LOGIN = aws ecr get-login-password --region us-east-1
+docker login --username AWS --password-stdin 970547375353.dkr.ecr.us-east-1.amazonaws.com
 
-  # Pull Docker Images from AWS ECR
-  Write-Output "Pulling Docker Images from AWS ECR..."
-  docker pull 970547375353.dkr.ecr.us-east-1.amazonaws.com/mcrp-api-image-repo:latest
-  docker pull 970547375353.dkr.ecr.us-east-1.amazonaws.com/mcrp-ui-image-repo:latest
+# Pull Docker Images from AWS ECR
+Write-Output "Pulling Docker Images from AWS ECR..."
+docker pull 970547375353.dkr.ecr.us-east-1.amazonaws.com/mcrp-api-image-repo:latest
+docker pull 970547375353.dkr.ecr.us-east-1.amazonaws.com/mcrp-ui-image-repo:latest
 
-  # Run API Container
-  Write-Output "Starting API Container..."
-  docker run -d --name mcrp-api-container -p 5000:5000 970547375353.dkr.ecr.us-east-1.amazonaws.com/mcrp-api-image-repo:latest
+# Run API Container
+Write-Output "Starting API Container..."
+docker run -d --name mcrp-api-container -p 5000:5000 970547375353.dkr.ecr.us-east-1.amazonaws.com/mcrp-api-image-repo:latest
 
-  # Run UI Container
-  Write-Output "Starting UI Container..."
-  docker run -d --name mcrp-ui-container -p 80:80 970547375353.dkr.ecr.us-east-1.amazonaws.com/mcrp-ui-image-repo:latest
+# Run UI Container
+Write-Output "Starting UI Container..."
+docker run -d --name mcrp-ui-container -p 80:80 970547375353.dkr.ecr.us-east-1.amazonaws.com/mcrp-ui-image-repo:latest
 
-  Write-Output "Docker containers for API and UI are now running!"
-  EOF
+Write-Output "Docker containers for API and UI are now running!"
+EOF
   )
 }
-
-
-resource "azurerm_virtual_machine_extension" "docker_install" {
-  name                 = "docker-install"
-  virtual_machine_id   = azurerm_windows_virtual_machine.vm.id
-  publisher            = "Microsoft.Compute"
-  type                 = "CustomScriptExtension"
-  type_handler_version = "1.10"
-
-  settings = <<SETTINGS
-    {
-      "fileUris": ["https://raw.githubusercontent.com/CAA900NBB-Cloud-Nexus/MCRP-Cloud/refs/heads/Terraform-Azure/Terraform-Resource/install-docker.ps1"],
-      "commandToExecute": "powershell -ExecutionPolicy Unrestricted -File install-docker.ps1"
-    }
-  SETTINGS
-}
-
-
-
