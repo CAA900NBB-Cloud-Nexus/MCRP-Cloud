2bf828f9443a43fa10d23f2f49ab742736b56961
shri-5279 <77598004+shri-5279@users.noreply.github.com>
2025-03-13 22:57:07 -0400

    Image set Addition
    Added the virtual windows image for VM

--- Terraform-Resource/main.tf
+++ Terraform-Resource/main.tf
@@ -1,33 +1,11 @@
 provider "azurerm" {
   features {}
-
   subscription_id = "975f2f0a-fb15-4ffc-8a94-e3e778f2ab22"  
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = "my-resource-group"
   location = "East US"
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "vm-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "public-ip-config"
-    subnet_id                     = azurerm_subnet.public_subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
-  }
-}
-
-resource "azurerm_public_ip" "vm_public_ip" {
-  name                = "vm-public-ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
 }
 
 resource "azurerm_virtual_network" "vnet" {
@@ -42,6 +20,27 @@ resource "azurerm_subnet" "public_subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_public_ip" "vm_public_ip" {
+  name                = "vm-public-ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "vm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "public-ip-config"
+    subnet_id                     = azurerm_subnet.public_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm_public_ip.id
+  }
 }
 
 resource "azurerm_network_security_group" "nsg" {
@@ -80,17 +79,25 @@ resource "azurerm_network_interface_security_group_association" "nsg_association
 }
 
 resource "azurerm_windows_virtual_machine" "vm" {
-  name                = "my-windows-vm"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size               = "Standard_B2ms"
-  admin_username      = "adminuser"
-  admin_password      = "P@ssw0rd123!" # Change this to a secure password
+  name                  = "my-windows-vm"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  size                  = "Standard_B2ms"
+  admin_username        = "adminuser"
+  admin_password        = "P@ssw0rd123!" # Change this to a secure password
   network_interface_ids = [azurerm_network_interface.nic.id]
 
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
+  }
+
+  # ✅ FIXED: Added `source_image_reference`
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter-smalldisk"
+    version   = "latest"
   }
 
   custom_data = base64encode(<<EOF
@@ -126,7 +133,3 @@ resource "azurerm_windows_virtual_machine" "vm" {
   EOF
   )
 }
-
-
-
-
